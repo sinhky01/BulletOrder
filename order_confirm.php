d74b706914c810a456fa976bf65502e92ad1b34b
shopab01 <shopab01@gettysburg.edu>
2018-04-24 15:52:17 -0400

    matched theme, minor aesthetic changes

--- order_confirm.php
+++ order_confirm.php
@@ -7,10 +7,24 @@ $list = $_POST['cborder'];
 <head>
   <title>Bullet order Menu</title>
   <style type="text/css">
-  table, th, td {
-    border: 1px solid black;
+  th, td {
+	color: #002F6C;
+	padding: 20px;
     align: center;
+	border: none;
+
   }
+  table {
+	border-collapse: collapse;
+	border: none;
+	align: center;
+  }
+  .submit_button{
+    float: center;
+  }
+	body {
+	background-color: #dbdfe5;
+	}
   .submit_button{
     float: center;
   }
@@ -19,14 +33,10 @@ $list = $_POST['cborder'];
 
 <body>
   <form method="post" action="order_confirm.php">
-    <table align="center" style="border: 1px solid black;width:60%;cellspacing = 0; cellpadding = 5">
-      <tr>
-        <th>Order List</th>
-        <th>Price</th>
-        <th>Remove</th>
-      </tr>
+    <table align="center" style="border:none;width:80%;cellspacing = 0; cellpadding = 5">
             <?php
             if ($list != null){
+              		print " <tr> <th>Order List</th> <th>Price</th> <th>Remove</th> </tr>";
               $total_price = 0;
               foreach ($list AS $id){
                 $qStr = "SELECT name, price FROM food WHERE food.food_id = ".$id.";";
@@ -43,7 +53,7 @@ $list = $_POST['cborder'];
               }
             }
             else{
-              print "You have not select anything! ";
+              print "You have not selected anything! ";
               // $url = phpLink('order_main.php');
               // header( "Location: $url" );
             }
