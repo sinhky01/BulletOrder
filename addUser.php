84b7f9efdac763ae4b828ebc605cea2e1ec9bc7c
sinhky01 <37851299+sinhky01@users.noreply.github.com>
2018-05-09 14:14:14 -0400

    Update addUser.php

--- addUser.php
+++ addUser.php
@@ -5,7 +5,114 @@
 //addUser connects to the SQL database and adds the information from register.php
 include_once('db_connect.php');
 
-print_r($_POST);
+function phpLink($page){
+    $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    $link_element = explode("/", $actual_link);
+    $curr_page = $link_element[5];
+    $page_link = str_replace($curr_page,$page,$actual_link);
+    //echo $page_link;
+    return $page_link;
+}
+?>
+<HTML>
+	<HEAD>
+
+		<TITLE>Welcome</TITLE>
+
+		<META name="viewport" content="width=device-width, initial-scale=1">
+
+			<LINK rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+				<LINK rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+
+					<SCRIPT src="js/jquery-1.11.3.min.js"> </SCRIPT>
+					<SCRIPT src="js/bootstrap.min.js">     </SCRIPT>
+
+					<STYLE type="text/css">
+					/*for all paragraphs*/
+					p {
+						padding: 100px;
+						background: #002F6C;
+						color: white;
+						font-family: arial;
+						text-align: center;
+					}	
+					body {
+						color: #002F6C;
+					}
+					p.gray {
+						padding: 20px;
+						font-size: 15px;
+						font-weight: bold;
+						text-align: center;
+						background: #dbdfe5;
+						font-family: arial;
+					}
+					h1 {
+						text-align: center;
+						color: #002F6C;
+						font-family: arial;
+						font-size: 72px;
+					}
+					body {
+						text-align: center;
+						font-family: arial;
+					}
+					input {
+						margin-bottom: 8px;
+					}
+					form {
+						background: #002F6C;
+						font-family: arial;
+						color: white;
+						font-weight: bold;
+						padding: 250px;
+					}
+					td {
+						padding-top: 12px;
+						padding-bottom: 9px;
+					}
+					table td+td {
+						text-align: right;
+					}
+					table {
+						font-size: 20px;
+						width: 100%;
+					}
+					ul {
+						list-style-type: none;
+						margin: 0;
+						padding: 0;
+						overflow: hidden;	
+						color: white;	
+						background-color: #002F6C;
+					}
+					li {
+
+						float: right;	
+						background-color: #002F6C;
+						color: white;	
+
+					}
+					li a {
+						display: block;
+						padding: 20px 20px;
+						text-align: center;
+						color: white;
+					}
+					li a:hover {
+						background-color:  #dbdfe5;
+					}
+					.active {
+						background-color: orange;
+					}
+				</STYLE>
+			</HEAD>
+<BODY>
+<?php
+//Kyle Sinhart
+//Abagale Shope
+//Ruiwen Fu
+//addUser connects to the SQL database and adds the information from register.php
 
 $id = $_POST['id'];
 $name = $_POST['name'];
@@ -18,13 +125,13 @@ $passW = md5($passW);
 $key = $name . $email . date('mY');
 $key = md5($key);
 
-$add1 = "INSERT INTO customer VALUE(" . $id . ", '" . $name . "', '". $passW . "', " . $phonenum . ", '" . $email . "', 15, 100, 0, '" . $key . "', 1);";
+$add1 = "INSERT INTO customer VALUE(" . $id . ", '" . $name . "', '". $passW . "', " . $phonenum . ", '" . $email . "', 15, 100, 0, '" . $key . "', 1, 'logo.png', 0);";
 
 $result1 = $db->query($add1);
 //$result2 = $db->query($add2);
 
 if($result1 != FALSE){// && $result2 != FALSE){
-  print "<p>new user named " . $name . " has been added.</p>";
+  //print "<p>new user named " . $name . " has been added.</p>";
 
 
 
@@ -43,12 +150,12 @@ if($result1 != FALSE){// && $result2 != FALSE){
     echo "<p> A confirmation Email was not sent, Invalid user info.</p\n>";
     $url = phpLink('register.php');
     header( "Location: $url" );
-
+		
   }
   else {
     echo "<p> A confirmation Email was sent, please check your email.</p\n>";
-    $url = phpLink('landing.php');		
- 		print "<A href='" . $url . "' >Click here for landing page. ---</A>";
+		$url = phpLink('landing.php');		
+		print "<A href='" . $url . "' >Click here for landing page. ---</A>";
   }
 }
 else{
@@ -56,20 +163,6 @@ else{
   $url = phpLink('register.php?success=-1');
   header( "Location: $url" );
 }
-
-// print $add1;
-//print $add2;
-
-
-
 ?>
-<?php
-function phpLink($page){
-    $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-    $link_element = explode("/", $actual_link);
-    $curr_page = $link_element[5];
-    $page_link = str_replace($curr_page,$page,$actual_link);
-    echo $page_link;
-    return $page_link;
-}
-?>
+</BODY>
+</HTML>
